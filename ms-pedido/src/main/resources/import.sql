8db0bdaa8a57f8f7a4e3a9d212d6f6f8a23545fb
Ricardo Neto <ricardo123neto88@gmail.com>
2025-04-25 10:59:20 -0300

    feat: modifying import.sql

--- ms-pedido/src/main/resources/import.sql
+++ ms-pedido/src/main/resources/import.sql
@@ -1,1 +1,7 @@
 INSERT INTO tb_pedido(nome, cpf, data, status) VALUES ('Jon Snow', '12345678901', '2025-04-25', 'REALIZADO')
+INSERT INTO tb_pedido(nome, cpf, data, status) VALUES ('Ricardão', '12345678910', '2025-04-25', 'REALIZADO')
+
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES (2, 'Mouse sem fio Microsoft', 250, 1)
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES (1, 'Teclado sem fio Microsoft', 290, 1)
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES (1, 'Teclado sem fio Microsoft', 3599, 2)
+
