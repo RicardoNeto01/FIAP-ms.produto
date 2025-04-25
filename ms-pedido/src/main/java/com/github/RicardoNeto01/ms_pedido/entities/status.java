853eafb0e7aec5bba40c7049a18e3385a51eef07
Ricardo Neto <ricardo123neto88@gmail.com>
2025-04-25 09:48:16 -0300

    feat: add enum status

---
+++ ms-pedido/src/main/java/com/github/RicardoNeto01/ms_pedido/entities/status.java
@@ -0,0 +1,14 @@
+package com.github.RicardoNeto01.ms_pedido.entities;
+
+public enum status {
+
+    REALIZADO,
+    AGUARDANDO_PAGAMENTO,
+    PAGO,
+    CONFIRMADO,
+    PRONTO_PARA_ENTREGA,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE,
+    CANCELADO
+
+}
