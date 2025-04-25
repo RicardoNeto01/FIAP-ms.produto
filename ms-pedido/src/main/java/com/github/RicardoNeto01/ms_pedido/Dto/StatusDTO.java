e2e85c588a05b247437dee6dead5bf6a34e6fe03
Ricardo Neto <ricardo123neto88@gmail.com>
2025-04-25 11:00:50 -0300

    feat: add StatusDTO

---
+++ ms-pedido/src/main/java/com/github/RicardoNeto01/ms_pedido/Dto/StatusDTO.java
@@ -0,0 +1,13 @@
+package com.github.RicardoNeto01.ms_pedido.Dto;
+
+import com.github.RicardoNeto01.ms_pedido.entities.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class StatusDTO {
+    private Status status;
+}
