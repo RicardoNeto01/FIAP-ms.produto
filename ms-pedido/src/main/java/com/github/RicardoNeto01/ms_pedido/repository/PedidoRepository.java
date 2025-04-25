c3b08885002938e6cee9875f3629fe69d2fc31f8
Ricardo Neto <ricardo123neto88@gmail.com>
2025-04-25 11:43:18 -0300

    feat: Add PedidoRepository

---
+++ ms-pedido/src/main/java/com/github/RicardoNeto01/ms_pedido/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.github.RicardoNeto01.ms_pedido.repository;
+
+import com.github.RicardoNeto01.ms_pedido.Entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
