5cfda4d6ada79b27b8302cc361b56e8642d177c6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-25 09:37:30 -0300

    add: Project ms-pedido

---
+++ ms-pedido/src/test/java/com/github/RicardoNeto01/ms_pedido/MsPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RicardoNeto01.ms_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
