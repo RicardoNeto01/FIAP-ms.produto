c3e8ced659f382f95d57107e793153252df98612
Ricardo Neto <ricardo123neto88@gmail.com>
2025-04-25 11:07:35 -0300

    feat: add PedidoDTO

---
+++ ms-pedido/src/main/java/com/github/RicardoNeto01/ms_pedido/Dto/PedidoDTO.java
@@ -0,0 +1,31 @@
+package com.github.RicardoNeto01.ms_pedido.Dto;
+
+import com.github.RicardoNeto01.ms_pedido.entities.ItemDoPedido;
+import com.github.RicardoNeto01.ms_pedido.entities.Status;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PedidoDTO {
+
+    private Long id;
+    @NotEmpty(message = "Nome requerido")
+    @Size(min = 3, max = 100, message = "O nome deve ser entre 3 e 100 caracteres.")
+    private String nome;
+    @NotEmpty(message = "Cpf requerido.")
+    @Size(min = 11, max = 11, message = "O cpf deve ter entre 11 e 14 caracteres.")
+    private String cpf;
+    private LocalDate data;
+    private Status status;
+    private List<@Valid ItemDoPedido> itens = new ArrayList<>();
+}
